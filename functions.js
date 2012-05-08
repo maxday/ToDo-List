5ed008c8150dbe3c63cf37145393b8ea806702e1
Maxime DAVID <got2bex@gmail.com>
2012-05-08 23:28:04 +0200

    Landing page

---
+++ functions.js
@@ -0,0 +1,24 @@
+function isCorrectMail(inm) {
+	var reg = new RegExp (/^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/); // Typique e-mail 
+	return(reg.test(inm));
+} 
+
+function addMail() { 
+	var mail = $('#mail').val(); 
+	if ( isCorrectMail(mail) ) {
+			$.ajax({
+			   type: "GET",
+			   url: "reg.php",
+			   data: "mail=" + escape(mail),
+			   success: function(){
+			     alert( "Email enregistré ! Merci :)"); 
+			   }
+			 });
+		}
+		else {
+			alert("Email est incorrect !")
+		}
+}
+$(document).ready(function(){	
+	$('#submitbutton').click(addMail);
+});
