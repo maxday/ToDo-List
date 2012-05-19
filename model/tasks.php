53aa741284fd02f3ac7979659dbcff4f180a19dc
Maxime DAVID <got2bex@gmail.com>
2012-05-19 23:23:49 +0200

    test tasks.php

--- model/tasks.php
+++ model/tasks.php
@@ -1,28 +1,31 @@
 <?php
+
+include("../helper/utils.php");
+
+/* tested */
 function createTask($title, $dueDate, $priority, $isImportant, $tag, $user) {
-	$sql = "INSERT INTO MYTODO_TASK(dateCreated, title, dueDate, priority, isImportant, tag, user) VALUES (NOW(), ?, ?, ?, ?, ?, ?)"; 
-	$array = array($title, $dueDate, $priority, $isImportant, $tag, $user));
+	$sql = "INSERT INTO MYTODO_TASK(uuid, dateCreated, title, dueDate, priority, isImportant, tag, user) VALUES (?, NOW(), ?, ?, ?, ?, ?, ?)"; 
+	$uniqId = uniqid("",true);
+	$array = array($uniqId, $title, $dueDate, $priority, $isImportant, $tag, $user);
 	
 	launchQuery($sql, $array);
+	return $uniqId;
 }
 
-function verifyTask($uuid, $user) {
-	$vConnect = new Connect();
-	$vConnect->mConnect();
-
-	$sql = "SELECT title FROM MYTODO_TASK WHERE uiid=? AND user=?";
+/* tested */
+/* PRIVATE */
+function isTaskBelongsToUser($uuid, $user) {
+	$vConnect = connect();
+	$sql = "SELECT uuid FROM MYTODO_TASK WHERE uuid=? AND user=?";
 	$prepared_statement = $vConnect->prepare($sql);
-
-	if ($res = $prepared_statement->execute($uuid, $user);){
-	   if ($res->fetchColumn() > 0)
-		  	return true;
-		else
-			return false;
+	
+	if($prepared_statement->execute(array($uuid, $user)) == true) {
+		return $prepared_statement->rowCount();
 	}
 }
-
+/* tested */
 function deleteTask($uuid, $user) {
-	if (verifyTask($uuid, $user)) {
+	if (isTaskBelongsToUser($uuid, $user)) {
 	  	$sql = "UPDATE MYTODO_TASK SET dateDeleted=NOW() WHERE uuid=?";
 		$array = array($uuid);
 		launchQuery($sql, $array);
@@ -31,8 +34,9 @@ function deleteTask($uuid, $user) {
 		echo("Alerte");
 }
 
+/* tested */
 function completeTask($uuid, $user) {
-	if (verifyTask($uuid, $user)) {
+	if (isTaskBelongsToUser($uuid, $user)) {
 	  	$sql = "UPDATE MYTODO_TASK SET dateCompleted=NOW() WHERE uuid=?";
 		$array = array($uuid);
 		launchQuery($sql, $array);
@@ -41,8 +45,9 @@ function completeTask($uuid, $user) {
 		echo("Alerte");
 }
 
+/*tested*/
 function updateTaskTitle($uuid, $user, $title) {
-	if (verifyTask($uuid, $user)) {
+	if (isTaskBelongsToUser($uuid, $user)) {
 	  	$sql = "UPDATE MYTODO_TASK SET title=? WHERE uuid=?";
 		$array = array($title, $uuid);
 		launchQuery($sql, $array);
@@ -51,8 +56,9 @@ function updateTaskTitle($uuid, $user, $title) {
 		echo("Alerte");
 }
 
+/*tested*/
 function updateTaskDueDate($uuid, $user, $dueDate) {
-	if (verifyTask($uuid, $user)) {
+	if (isTaskBelongsToUser($uuid, $user)) {
 	  	$sql = "UPDATE MYTODO_TASK SET dueDate=? WHERE uuid=?";
 		$array = array($dueDate, $uuid);
 		launchQuery($sql, $array);
@@ -61,18 +67,20 @@ function updateTaskDueDate($uuid, $user, $dueDate) {
 		echo("Alerte");
 }
 
+/*tested*/
 function updateTaskPriority($uuid, $user, $priority) {
-	if (verifyTask($uuid, $user)) {
+	if (isTaskBelongsToUser($uuid, $user)) {
 	  	$sql = "UPDATE MYTODO_TASK SET priority=? WHERE uuid=?";
-		$array = array($title, $uuid);
+		$array = array($priority, $uuid);
 		launchQuery($sql, $array);
 	}
 	else
 		echo("Alerte");
 }
 
+/*tested*/
 function updateTaskImportant($uuid, $user, $isImportant) {
-	if (verifyTask($uuid, $user)) {
+	if (isTaskBelongsToUser($uuid, $user)) {
 	  	$sql = "UPDATE MYTODO_TASK SET isImportant=? WHERE uuid=?";
 		$array = array($isImportant, $uuid);
 		launchQuery($sql, $array);
