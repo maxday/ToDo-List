d1c4dc84dd28dead63a022b362f28af3c30d6ba8
Maxime DAVID <got2bex@gmail.com>
2012-05-31 23:19:19 +0200

    swith li/ul

--- view/tasksList.php
+++ view/tasksList.php
@@ -4,12 +4,12 @@
 	$array = seeTasks($_SESSION['uuid']);
 	
   for($i = 0; $i < count($array); $i++){
-	  echo "<li>";
-		  echo "<ul class='singleTitle'>"; echo($array[$i] -> title); echo "</ul>";
-		  echo "<ul class='singleDueDate'>"; echo($array[$i] -> dueDate); echo "</ul>";
-		  echo "<ul class='singlePriority'>"; echo($array[$i] -> priority); echo "</ul>";
-		  echo "<ul class='singleIsImportant'>"; echo($array[$i] -> isImportant); echo "</ul>";
-		  echo "<ul class='singleTag'>"; echo($array[$i] -> tag); echo "</ul>";
-	  echo "</li>";
+	  echo "<ul>";
+		  echo "<li class='singleTitle'>"; echo($array[$i] -> title); echo "</li>";
+		  echo "<li class='singleDueDate'>"; echo($array[$i] -> dueDate); echo "</li>";
+		  echo "<li class='singlePriority'>"; echo($array[$i] -> priority); echo "</li>";
+		  echo "<li class='singleIsImportant'>"; echo($array[$i] -> isImportant); echo "</li>";
+		  echo "<li class='singleTag'>"; echo($array[$i] -> tag); echo "</li>";
+	  echo "</ul>";
   }
 ?>
