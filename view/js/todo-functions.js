a77e54f24a8d9382a468ad3c59cc1de43ab0fcb5
Maxime DAVID <got2bex@gmail.com>
2012-05-31 14:39:13 +0200

    1st steph to compute String Task

--- view/js/todo-functions.js
+++ view/js/todo-functions.js
@@ -6,13 +6,29 @@
 
 var lastTagClicked = null;
 var priority = -2;
+var isImportant = false;
 
 $(document).ready(function () {
+	
 	$("#newTask").bind("submit", function(event){  
+
+        if($("#checkimp").attr('checked')) {
+			isImportant = true;
+		}
+		else
+			isImportant = false;
 
 		var lastDateChosen = $("#date").val();
 		var title = $('#text_field_task').val();
 		var url = './../ws/addTask.php';
+
+		
+		var complexTask = computeTask(title, lastTagClicked, priority, isImportant, lastDateChosen);
+		
+		alert(complexTask);
+
+		
+
 		$.post(url, { title_task: title},
 			function (data) {
 				if(data == "1") {
@@ -23,17 +39,38 @@ $(document).ready(function () {
 			}
 		); 
 	});
+	
 	/* SAVE TAG */
 	$(".tagButton").bind("click", function(event){
 	    lastTagClicked = $(this).attr("value");
-		alert("Dernier tag clické = " + lastTagClicked);
 		event.preventDefault();
 	});
 	
 });
  
-/* sauvegarde du tag */
 
-$(".tagButton").bind("click", function(event){
-	alert("kikoo");  
-}); 
+
+function computeTask(title, lastTagClicked, priority, isImportant, lastDateChosen) {
+	var toReturn = title;
+	if(lastTagClicked != null)
+		toReturn += " -l " + lastTagClicked;
+	if(priority != null)
+		toReturn += " " + computePriorityPrefix(priority);
+	if(isImportant)
+		toReturn += " -i";
+	if(lastDateChosen != "")
+		toReturn += " -d " + lastDateChosen;
+	return toReturn;
+}
+
+function computePriorityPrefix(priority) {
+	if(priority == '-2')
+		return "-pp";
+	if(priority == '-1')
+		return "-p";
+	if(priority == '+1')
+		return "+p";
+	if(priority == '+2')
+		return "+pp";	
+	return "";	
+}
