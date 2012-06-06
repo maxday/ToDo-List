169feebab464b79f8fea98fe71b1e90066966b57
Maxime DAVID <got2bex@gmail.com>
2012-06-06 09:59:43 +0200

    display date FR in taskList

--- view/tasksList.php
+++ view/tasksList.php
@@ -13,7 +13,7 @@ tiens ton id : <?php echo $_SESSION['uuid'] ?>
 	      $uuid = str_replace('.','',$bdd_uuid);
 
 		  echo "<li class='task' id=$uuid bdd_id=$bdd_uuid><span class='singleTitle taskcolumn'>"; echo($array[$i] -> title); echo "</span>";
-		  echo "<span class='singleDueDate taskcolumn'>"; echo($array[$i] -> dueDate); echo "</span>";
+		  echo "<span class='singleDueDate taskcolumn'>"; echo(datefr($array[$i] -> dueDate)); echo "</span>";
 		  echo "<span class='singlePriority taskcolumn'>"; 
 		  $nbImg = '';
 		  for ( $j = 0 ; $j < $array[$i] -> priority ; $j++) {
