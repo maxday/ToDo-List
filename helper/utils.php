6f4940386a923c469831549bee3877768a65e385
Christian PATRY <christian.patry@etu.utc.fr>
2012-06-15 11:47:43 +0200

    Correction du probleme des dates, fonctionne désormais avec :
    12-04-2005
    12/04/2005
    12042005

--- helper/utils.php
+++ helper/utils.php
@@ -13,7 +13,7 @@ function launchQuery($sql, $array) {
 
 function datefr($date) { 
 	if ( !empty($date)) {
-		$split = preg_split("!-!",$date); 
+		$split = preg_split("!-!",$date);
 		$annee = $split[0]; 
 		$mois = $split[1]; 
 		$jour = $split[2]; 
@@ -24,12 +24,29 @@ function datefr($date) {
 	return "$jour"."-"."$mois"."-"."$annee"; 
 }
 
-function dateen($date) { 
-	if ( !empty($date)) {	
-		$split = preg_split("!-!",$date); 
-		$jour = $split[0]; 
-		$mois = $split[1]; 
-		$annee = $split[2]; 
+function dateen($date) {
+		echo $date;
+	if (!empty($date)) {
+		$pos = strpos($date, '-');
+		$pos2 = strpos($date, '/');
+		
+		if($pos > 0){
+			$split = preg_split("!-!",$date);
+			$jour = $split[0]; 
+			$mois = $split[1]; 
+			$annee = $split[2]; 
+		}
+		elseif ($pos2 > 0){
+			$split = preg_split("!/!",$date);
+			$jour = $split[0]; 
+			$mois = $split[1]; 
+			$annee = $split[2]; 
+		}
+		else{
+		  $jour = substr($date, 0, 2);
+		  $mois = substr($date, 2, 2);
+		  $annee = substr($date, 4, 4);
+		}
 	}
 	else {
 		$annee = '0000'; $mois = '00' ; $jour = '00';
